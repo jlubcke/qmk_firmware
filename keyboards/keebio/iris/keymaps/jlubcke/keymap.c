cdb618e9b8ca4b310f7bd2e4af798ba9e9158bee
Johan Lübcke <johan.lubcke@trioptima.com>
2022-08-17 11:31:16 +0200

    wip

--- keyboards/keebio/iris/keymaps/jlubcke/keymap.c
+++ keyboards/keebio/iris/keymaps/jlubcke/keymap.c
@@ -68,11 +68,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                               `--------------------'  `--------------------'
  */
     [_LOWER] = LAYOUT(
-    KC_NO,   KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,                     KC_NO,   KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
-    KC_TAB,  KC_EXLM,      KC_AT,        KC_HASH,      KC_DLR,       KC_PERC,                   KC_CIRC, KC_7,         KC_8,         KC_9,         KC_AMPR,      KC_TRNS,
-    CTL_ESC, LSFT_T(KC_1), LCTL_T(KC_2), LALT_T(KC_3), LGUI_T(KC_4), KC_5,                      KC_6,    RGUI_T(KC_4), RALT_T(KC_5), RCTL_T(KC_6), RSFT_T(KC_0), KC_TRNS ,
-    KC_TRNS, KC_PLUS,      KC_EQL,       KC_UNDS,      KC_MINS,      KC_DEL,  KC_TRNS, KC_TRNS, KC_0,    KC_1,         KC_2,         KC_3,         KC_ASTR,      KC_TRNS ,
-                                                       KC_LALT,      KC_LGUI, KC_TRNS, KC_TRNS, KC_RGUI, KC_RALT
+    KC_NO,   KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,                    KC_NO,   KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
+    KC_TAB,  KC_EXLM,      KC_AT,        KC_HASH,      KC_DLR,       KC_PERC,                  KC_CIRC, KC_7,         KC_8,         KC_9,         KC_AMPR,      KC_TRNS,
+    CTL_ESC, LSFT_T(KC_1), LCTL_T(KC_2), LALT_T(KC_3), LGUI_T(KC_4), KC_5,                     KC_6,    RGUI_T(KC_4), RALT_T(KC_5), RCTL_T(KC_6), RSFT_T(KC_0), KC_TRNS ,
+    KC_TRNS, KC_PLUS,      KC_EQL,       KC_UNDS,      KC_MINS,      KC_DEL,  KC_DEL, KC_BSPC, KC_0,    KC_1,         KC_2,         KC_3,         KC_ASTR,      KC_TRNS ,
+                                                       KC_LALT,      KC_LGUI, KC_ENT, KC_SPC,  KC_RGUI, KC_RALT
     ),
 
 /*
@@ -92,10 +92,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                               `--------------------'  `--------------------'
  */
     [_UPPER] = LAYOUT(
-     KC_NO,   KC_NO,         KC_NO,         KC_NO,         KC_NO,         KC_NO,                     KC_NO,   KC_NO,           KC_NO,            KC_NO,           KC_NO,           KC_NO,
-     KC_TAB,  KC_F1,         KC_F2,         KC_F3,         KC_F4,         KC_F5,                     KC_PGUP, KC_HOME,         KC_UP,            KC_END,          KC_INS,          KC_BSLS,
-     CTL_ESC, LSFT_T(KC_F6), LCTL_T(KC_F7), LALT_T(KC_F8), LGUI_T(KC_F9), KC_F10,                    KC_PGDN, RGUI_T(KC_LEFT), RALT_T(KC_DOWN) , RCTL_T(KC_RGHT), RSFT_T(KC_LCBR), KC_RCBR,
-     KC_TRNS, KC_F1,         KC_F12,        KC_TRNS,       KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_BSPC, KC_LPRN,         KC_RPRN,          KC_LBRC,         KC_RBRC,         KC_TRNS,
-                                                           KC_LALT,       KC_LGUI, KC_TRNS, KC_TRNS, KC_RGUI, KC_RALT
+     KC_NO,   KC_NO,         KC_NO,         KC_NO,         KC_NO,         KC_NO,                    KC_NO,   KC_NO,           KC_NO,            KC_NO,           KC_NO,           KC_NO,
+     KC_TAB,  KC_F1,         KC_F2,         KC_F3,         KC_F4,         KC_F5,                    KC_PGUP, KC_HOME,         KC_UP,            KC_END,          KC_INS,          KC_BSLS,
+     CTL_ESC, LSFT_T(KC_F6), LCTL_T(KC_F7), LALT_T(KC_F8), LGUI_T(KC_F9), KC_F10,                   KC_PGDN, RGUI_T(KC_LEFT), RALT_T(KC_DOWN) , RCTL_T(KC_RGHT), RSFT_T(KC_LCBR), KC_RCBR,
+     KC_TRNS, KC_F1,         KC_F12,        KC_TRNS,       KC_TRNS,       KC_TRNS, KC_DEL, KC_BSPC, KC_BSPC, KC_LPRN,         KC_RPRN,          KC_LBRC,         KC_RBRC,         KC_TRNS,
+                                                           KC_LALT,       KC_LGUI, KC_ENT, KC_SPC,  KC_RGUI, KC_RALT
     ),
 };
